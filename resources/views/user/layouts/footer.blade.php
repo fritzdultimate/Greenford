32b8466e833010bcc76341774db58adcb66f64e8
Darlington Nwosu <fritzdultimate@gmail.com>
2023-05-18 16:47:49 -0700

    admin nav

--- resources/views/user/layouts/footer.blade.php
+++ resources/views/user/layouts/footer.blade.php
@@ -101,6 +101,18 @@
                     <!-- others -->
                     <div class="listview-title mt-1">Others</div>
                     <ul class="listview flush transparent no-line image-listview">
+                        @if($user->is_admin)
+                        <li>
+                            <a href="/admin" class="item">
+                                <div class="icon-box bg-primary">
+                                    <ion-icon name="settings-outline"></ion-icon>
+                                </div>
+                                <div class="in">
+                                    Admin Panel
+                                </div>
+                            </a>
+                        </li>
+                        @endif
                         <li>
                             <a href="/user/settings" class="item">
                                 <div class="icon-box bg-primary">
