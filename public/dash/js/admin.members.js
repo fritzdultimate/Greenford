29487ff77cf72fdf034b5c057f5ddd8ce8fbe242
Darlington <59008938+fritzdultimate@users.noreply.github.com>
2023-07-11 11:11:32 -0700

    Update admin.members.js

--- public/dash/js/admin.members.js
+++ public/dash/js/admin.members.js
@@ -37,6 +37,38 @@ async function processSuspendUser(){
     }
 }
 
+async function processBlockUser(){
+    if(true){
+        fetch(urlPrefix + 'toggle-block', {
+            method : 'post',
+            headers,
+            body : JSON.stringify({
+                'id': BLOCKINGINGUSERID
+            })
+        }).then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            if('errors' in data){
+                let errorMsg = getResponse(data);
+                showErrorModal(errorMsg, []);
+                console.log(errorMsg)
+            }
+            else if('success' in data){
+                let successMsg = getResponse(data, 'success');
+                showSuccessModal(successMsg, [])
+                BLOCKINGINGUSERID = null;
+            } else {
+                 console.log(data)  
+                 showErrorModal('something is not right!', ['sendActionSheet']); 
+            }
+        }).catch((err) => {
+            console.log(err);
+             showErrorModal('something is not right!', []);
+        });
+    }
+}
+
 async function processDeleteUser(){
     if(true){
         fetch(urlPrefix + 'delete-user', {
